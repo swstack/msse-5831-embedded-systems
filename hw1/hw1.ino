54bd988d6d0cbfb58fa16b5b18f7aa697a97121f
Stephen Stack <sstack@punchthrough.com>
2016-01-28 21:21:48 -0600

    hw1: first attempt at solution...kinda sorta works

--- hw1/hw1.ino
+++ hw1/hw1.ino
@@ -16,6 +16,9 @@ AStar32U4ButtonA buttonA;
 AStar32U4ButtonB buttonB;
 AStar32U4ButtonC buttonC;
 
+int stateButtonA = 0;
+int stateButtonC = 0;
+
 void setup()
 {
 
@@ -23,15 +26,59 @@ void setup()
 
 void loop()
 {
-  while (1)
-  {
-    if (buttonA.getSingleDebouncedRelease())
-    {
-      // blink LED
+  if (buttonA.getSingleDebouncedRelease()) {
+    if (stateButtonA < 2) {
+      stateButtonA++;
+    } else {
+      stateButtonA = 0;
+    }
+  }
+
+  if (buttonC.getSingleDebouncedRelease()) {
+    if (stateButtonC < 2) {
+      stateButtonC++;
+    } else {
+      stateButtonC = 0;
+    }
+  }
+
+  updateGreenLEDState(stateButtonA);
+  updateYellowLEDState(stateButtonC);
+}
+
+void updateGreenLEDState(int stateButtonA) {
+  Serial.println(stateButtonA);
+  switch(stateButtonA) {
+
+    case 1:  // solid on
+      ledGreen(1);
+      break;
+
+    case 2:  // blink at 2hz
       ledGreen(1);
       delay(200);
       ledGreen(0);
       break;
-    }
+
+    default:  // off
+      ledGreen(0);
+  }
+}
+
+void updateYellowLEDState(int stateButtonC) {
+  switch(stateButtonC) {
+
+    case 1:  // solid on
+      ledYellow(1);
+      break;
+
+    case 2:  // blink at 2hz
+      ledYellow(1);
+      delay(200);
+      ledYellow(0);
+      break;
+
+    default:  // off
+      ledYellow(0);
   }
 }
