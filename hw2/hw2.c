da88560ce569f5accaca28863ad2f2337eb2e6c4
Stephen Stack <sstack@punchthrough.com>
2016-02-08 19:08:17 -0600

    hw: start of hw2

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -1,3 +1,19 @@
+/*
+
+Key datasheet info:
+
+Button A
+  Pin = PB3
+  Interrupt = PCINT3
+
+Button B
+  Pin = PD5
+
+Button C
+  Pin = PB0
+
+*/
+
 #define F_CPU 16000000
 #include <avr/io.h>
 #include <util/delay.h>
@@ -7,19 +23,41 @@
 // Button B = PD5
 // Button C = PB0
 
-int main()
-{
-  DDRC |= (1 << DDC7);    // Make pin 13 be an output.
+int main() {
+  // Configure buttons as input
+  // Note: "&= ~" is an idiomatic way to put the bit to 0
+  DDRB &= ~(1 << DDB3);  // Button A
+  DDRB &= ~(1 << DDB0);  // Button C
+
+  // Enable Button A pull-up resistor
+  PORTB |= (1 << PORTB3);
+
+  // Pin Change Interrupt Control Register = 1
+  // Any change on any enabled PCING7..0 will can an interrupt.
+  PCICR = 1;
+
+  // Enable interrupts on Button A (PCINT3) and Button C (PCINT0)
+  PCMSK0 = 0b00001000;
+
+  // Configures yellow LED pin as output
+  DDRC |= (1 << DDC7);
+
+  // Enable all interrupts
+  sei();
+
   while(1)
   {
-    PORTC |= (1 << PORTC7);   // Turn the LED on.
-    _delay_ms(500);
-    PORTC &= ~(1 << PORTC7);  // Turn the LED off.
-    _delay_ms(500);
+
   }
 }
 
-ISR(PCINT3_vect)
-{
-// user code here
+void flashYellow() {
+  PORTC = (1 << PORTC7);
+  _delay_ms(1000);
+  PORTC = 0;
+  _delay_ms(1000);
+}
+
+ISR(PCINT0_vect) {
+  flashYellow();
 }
