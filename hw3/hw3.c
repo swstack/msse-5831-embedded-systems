ad275d32d05422cdab07ece85c1fa44ddddf4dbb
Stephen Stack <sstack@punchthrough.com>
2016-02-10 20:48:38 -0600

    hw3: helper functions for all LEDs and setup 500hz timer

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -2,20 +2,34 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdbool.h>
 
-// Global variables
-uint32_t ms_count = 0;
+typedef enum {
+  OFF,
+  ON
+} led_state;
 
 // Function prototypes
 void flashGreen();
 void flashYellow();
+void yellow(led_state);
+void red(led_state);
+void green(led_state);
+
+// Global variables
+uint32_t ms_count = 0;
+led_state led_state_green = OFF;
+led_state led_state_yellow = OFF;
+led_state led_state_red = OFF;
+
+// ISRs
+ISR(TIMER3_COMPA_vect) {
+  // Every 500hz
+}
 
 ISR(TIMER1_COMPA_vect) {
+  // Every 1000hz
   ms_count++;
-
-  if (ms_count == 1000) {
-    ms_count = 0;
-  }
 }
 
 void initLEDs() {
@@ -24,44 +38,77 @@ void initLEDs() {
 
   // Configures green LED as output
   DDRD |= (1 << DDD5);
+
+  // configure red LED as output
+  DDRB |= (1 << DDB0);
 }
 
-void initTimers() {
+void init500hzTimer() {
+  // Setup CTC mode (Clear Timer Compare)
+  TCCR3A = 0;  // Bits 0 and 1 to value 0
 
+  // Setup prescaler value to 1024
+  TCCR3B = (1 << CS30) | (1 << CS31) | (1 << WGM32);
+
+  // Set TOP rollover to 15625
+  OCR3A = 500;
+
+  // Enable Output Compare A Match Interrupt
+  TIMSK3 = (1 << OCIE3A);
+}
+
+void init1000hzTimer() {
   // Setup CTC mode (Clear Timer Compare)
   TCCR1A = 0;  // Bits 0 and 1 to value 0
 
-  // Setup prescaler value
+  // Setup prescaler value to 64
   TCCR1B = (1 << CS10) | (1 << CS11) | (1 << WGM12);
 
-  // Set TOP (OCR1A)
+  // Set TOP rollover to 250
   OCR1A = 250;
 
-  // Enable timer interrupt
+  // Enable Output Compare A Match Interrupt
   TIMSK1 = (1 << OCIE1A);
-
 }
 
-void flashYellow() {
-  PORTC |= (1 << PORTC7);  // Turn on (set high)
-  _delay_ms(1000);
-  PORTC &= ~(1 << PORTC7);  // Turn off (set low)
+void yellow(led_state state) {
+  if (state == ON) {
+    PORTC |= (1 << PORTC7);  // Set high
+  } else {
+    PORTC &= ~(1 << PORTC7);  // Set low
+  }
 }
 
-void flashGreen() {
-  PORTD &= ~(1 << PORTD5);  // Turn on (set low)
-  _delay_ms(1000);
-  PORTD |= (1 << PORTD5);  // Turn off (set high)
+void green(led_state state) {
+  if (state == ON) {
+    PORTD &= ~(1 << PORTD5);  // Set low
+  } else {
+    PORTD |= (1 << PORTD5);  // Set high
+  }
 }
 
+void red(led_state state) {
+  if (state == ON) {
+    PORTB &= ~(1 << PORTB0);  // Set low
+  } else {
+    PORTB |= (1 << PORTB0);  // Set high
+  }
+}
 
 int main() {
-  initTimers();
+  init500hzTimer();
+  init1000hzTimer();
   initLEDs();
   sei();
 
   while (1) {
-    flashGreen();
+    red(ON);
+    green(ON);
+    yellow(ON);
+    _delay_ms(1000);
+    red(OFF);
+    green(OFF);
+    yellow(OFF);
     _delay_ms(1000);
   }
 
