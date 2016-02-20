87e6b1fb8128369175d9ff6f5a5e8275e2b65844
Stephen Stack <sstack@punchthrough.com>
2016-02-20 15:52:29 -0600

    common: pull out helper functions into common library

---
+++ common/common.c
@@ -0,0 +1,137 @@
+#include <avr/interrupt.h>
+#include "common.h"
+
+// Global variables
+uint32_t uptime_ms = 0;
+led_state led_state_green = OFF;
+led_state led_state_yellow = OFF;
+led_state led_state_red = OFF;
+
+void initButtons() {
+  // Initialize Button A as input
+  DDRB &= ~(1 << DDB3);
+
+  // Enable Button A pull-up resistor
+  PORTB |= (1 << PORTB3);
+}
+
+void initLEDs() {
+  // Configure on-board LED pins
+  DDRC |= (1 << DDC7);  // yellow
+  DDRD |= (1 << DDD5);  // green
+  DDRB |= (1 << DDB0);  // red
+
+  // Configure GPIO header pins
+  DDRD |= (1 << DDD6);  // red
+  DDRB |= (1 << DDB6);  // green
+}
+
+void init1hzTimer() {
+  // Setup CTC mode (Clear Timer Compare)
+  TCCR3A = 0;  // Bits 0 and 1 to value 0
+
+  // Setup prescaler value to 1024
+  TCCR3B = (1 << CS30) | (1 << CS32) | (1 << WGM32);
+
+  // Set TOP rollover to 15625
+  OCR3A = 15625;
+
+  // Enable Output Compare A Match Interrupt
+  TIMSK3 = (1 << OCIE3A);
+}
+
+void init1000hzTimer() {
+  // Setup CTC mode (Clear Timer Compare)
+  TCCR1A = 0;  // Bits 0 and 1 to value 0
+
+  // Setup prescaler value to 64
+  TCCR1B = (1 << CS10) | (1 << CS11) | (1 << WGM12);
+
+  // Set TOP rollover to 250
+  OCR1A = 250;
+
+  // Enable Output Compare A Match Interrupt
+  TIMSK1 = (1 << OCIE1A);
+}
+
+void toggleYellow() {
+  if (led_state_yellow == ON) {
+    yellow(OFF);
+  } else {
+    yellow(ON);
+  }
+}
+
+void toggleGreen() {
+  if (led_state_green == ON) {
+    green(OFF);
+  } else {
+    green(ON);
+  }
+}
+
+void toggleRed() {
+  if (led_state_red == ON) {
+    red(OFF);
+  } else {
+    red(ON);
+  }
+}
+
+void yellow(led_state state) {
+  if (state == ON) {
+    PORTC |= (1 << PORTC7);  // Set high
+  } else {
+    PORTC &= ~(1 << PORTC7);  // Set low
+  }
+  led_state_yellow = state;
+}
+
+void green(led_state state) {
+  if (state == ON) {
+    PORTD &= ~(1 << PORTD5);  // Set low
+  } else {
+    PORTD |= (1 << PORTD5);  // Set high
+  }
+  led_state_green = state;
+}
+
+void red(led_state state) {
+  if (state == ON) {
+    PORTB &= ~(1 << PORTB0);  // Set low
+  } else {
+    PORTB |= (1 << PORTB0);  // Set high
+  }
+  led_state_red = state;
+}
+
+void gpioRed(led_state state) {
+  if (state == ON) {
+    PORTD |= (1 << PORTD6);  // Set high
+  } else {
+    PORTD &= ~(1 << PORTD6);  // Set low
+  }
+  led_state_red = state;
+}
+
+void gpioGreen(led_state state) {
+  if (state == ON) {
+    PORTB |= (1 << PORTB6);  // Set high
+  } else {
+    PORTB &= ~(1 << PORTB6);  // Set low
+  }
+  led_state_red = state;
+}
+
+int systemUptime() {
+  cli();
+  int cpy = uptime_ms;
+  sei();
+  return cpy;
+}
+
+void resetUptime() {
+  cli();
+  uptime_ms = 0;
+  sei();
+}
---
+++ common/common.h
@@ -0,0 +1,29 @@
+typedef enum { OFF, ON } led_state;
+
+/*
+    Init Functions
+*/
+void initLEDs();
+void init1hzTimer();
+void init1000hzTimer();
+void initButtons();
+
+/*
+    LED Functions
+*/
+void yellow(led_state);
+void red(led_state);
+void green(led_state);
+
+void toggleGreen();
+void toggleYellow();
+void toggleRed();
+
+void gpioRed(led_state);
+void gpioGreen(led_state);
+
+/*
+    Time Functions
+*/
+int systemUptime();
+void resetUptime();
