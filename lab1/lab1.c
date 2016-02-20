dd8fad21db3087b96b690c8f847bb580c67c4985
Stephen Stack <sstack@punchthrough.com>
2016-02-20 15:41:44 -0600

    lab1: initial lab1 C and Makefile that sets up VirtualSerial

---
+++ lab1/lab1.c
@@ -0,0 +1,28 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000
+#include <util/delay.h>
+#include <VirtualSerial.h>
+#include <stdio.h>
+#include <string.h>
+
+void init()
+{
+    SetupHardware();
+    sei();
+}
+
+int main()
+{
+    init();
+    uint8_t value = 0xFF;
+
+    while (1)
+    {
+        USB_Mainloop_Handler();
+
+        printf("[%p] %x\r\n", &value, value--);
+        _delay_ms(500);
+    }
+}
