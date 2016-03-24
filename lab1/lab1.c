4144c703f7bb748b4219859ecae9111d087083f0
Stephen Stack <sstack@punchthrough.com>
2016-03-23 20:41:01 -0500

    lab1: green LED task working

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -40,22 +40,19 @@ void handleGreenLedTask() {
 /*
   Main and Inits
 */
-void initLedTasks() {
-  // Configure Green LED GPIO as output
+void initGreenLedTask() {
   DDRB |= (1 << DDB6);
-
-  // Green LED Fast PWM config (OC1B)
-  TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-
-  // Green LED prescaler config
-  TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS12) | (1 << CS10);
+  TCCR1A |= (1 << COM1B1) | (1 << WGM11);
+  TCCR1B |= (1 << WGM13) | (1 << CS12) | (1 << CS10);
+  ICR1 = 1562;
+  OCR1B = ICR1 / 2;
 }
 
 void init() {
   SetupHardware();
   init1000hzTimer();
   initOnBoardLEDs();
-  initLedTasks();
+  initGreenLedTask();
   initInputButtons();
   sei();
 }
