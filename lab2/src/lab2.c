e43d64994ae229f0b27747d1073396d3d09e52a0
Stephen Stack <sstack@punchthrough.com>
2016-04-22 17:17:48 -0500

    lab2: initial motor control for forward/reverse

--- lab2/src/lab2.c
+++ lab2/src/lab2.c
@@ -3,9 +3,35 @@
 #include <stdlib.h>
 #include <common.h>
 
+void forward(int speed)
+{
+  PORTE |= _BV(PORTE2);
+  OCR1B = speed;
+}
+
+void reverse(int speed)
+{
+  PORTE &= ~(_BV(PORTE2));
+  OCR1B = speed;
+}
+
+void init() {
+  init_on_board_leds();
+
+  // Initialize motor
+  DDRB |= (1 << DDB6);  // Configure header pin 10 (motor) as output
+
+  // Timer 1: phase-correct PWM mode, prescaler of 1024
+  TCCR1A |= (1 << COM1B1) | (1 << WGM11);
+  TCCR1B |= (1 << WGM13) | (1 << CS10);
+
+  // Timer rollover @ 10hz
+  ICR1 = 0xFFF;
+}
+
 int main() {
 
-  init_on_board_leds();
+  init();
   flash_on_board_leds();
 
   while (1) {
