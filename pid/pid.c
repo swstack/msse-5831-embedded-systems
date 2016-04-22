66670378ac9070e5a24f467cf0a7246f39237122
Stephen Stack <sstack@punchthrough.com>
2016-04-22 11:00:56 -0500

    pid: add some comments to PID example

--- pid/pid.c
+++ pid/pid.c
@@ -10,9 +10,14 @@ int main() {
   // Configure header pin 10 (motor) as output
   DDRB |= (1 << DDB6);
 
+  // Phase correct PWM mode, prescaler of 1024
   TCCR1A |= (1 << COM1B1) | (1 << WGM11);
   TCCR1B |= (1 << WGM13) | (1 << CS12) | (1 << CS10);
+
+  // Timer rollover @ 10hz
   ICR1 = 1562;
+
+  // PWM/Motor changing state every 100ms
   OCR1B = ICR1 / 2;
 
   while(1) {
